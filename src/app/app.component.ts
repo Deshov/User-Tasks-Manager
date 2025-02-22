aeedc64abeecdce28ca4e1f619021c74b9c25540
Ivaylo Deshov <79753114+Deshov@users.noreply.github.com>
2025-02-22 23:53:12 +0200

    Added new component tasks.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,18 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header/header.component";
 import { UserComponent } from "./user/user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./components/tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = signal(DUMMY_USERS);
-
+  selectedUserId: string | null = null;
   onSelectUser(id:string){
-    console.log('User selected:', id);
+    this.selectedUserId = id;
   }
 }
