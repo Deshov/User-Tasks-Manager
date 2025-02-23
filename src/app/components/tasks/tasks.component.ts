c5738ad3a3b6fab7a5c57754dc21704931e92d1c
Ivaylo Deshov <79753114+Deshov@users.noreply.github.com>
2025-02-23 13:21:39 +0200

    Addded tasks component introduce Task model

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,13 +1,20 @@
 import { Component, input } from '@angular/core';
-
+import { TaskComponent } from "./task/task.component";
+import { DUMMY_TASKS } from "./dummy-tasks";
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+ name = input<string >();
  userid = input<string >();
- 
+
+ tasks = DUMMY_TASKS;
+
+ get userTasks() {
+   return this.tasks.filter(task => task.userId === this.userid());
+ }
 }
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component,computed, input, output } from '@angular/core';
 
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-};
+import { User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
