35d2be766470bb3a2d31f63d1fbf0d4b8cd3fc65
Ivaylo Deshov <79753114+Deshov@users.noreply.github.com>
2025-02-23 16:45:10 +0200

    Add new task component with dialog for task creation

---
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -0,0 +1,34 @@
+import { Component,input,output,signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Task } from '../task/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  close = output<void>();
+  newTask = output<Task>();
+  userid = input.required<string>();
+  enteredTItle= signal('');
+  enteredSummary = signal('');
+  enteredDate = signal('');
+
+  onCloseDialog() {
+    this.close.emit();
+  }
+  onCreateTask() {
+    this.newTask.emit(
+      {
+        id: Math.random().toString(), 
+        title: this.enteredTItle(), 
+        summary: this.enteredSummary(), 
+        dueDate: this.enteredDate(), 
+        userId: this.userid()
+      }
+    );
+  }
+}
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from "./dummy-tasks";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { Task } from "./task/task.model";
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
  name = input<string >();
  userid = input<string >();
-
+ showNewTask = false
  tasks = DUMMY_TASKS;
 
  get userTasks() {
@@ -19,5 +21,18 @@ export class TasksComponent {
  }
  completeTask(taskId?: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId)
+ }
+
+ showNewTaskDialog() {
+   this.showNewTask = true;
+ }
+
+ closeTaskDialog() {
+   this.showNewTask = false;
+ }
+
+ addTask(task: Task) {
+   this.tasks = [...this.tasks, task];
+   this.showNewTask = false;
  }
 }
